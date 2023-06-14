70286b9b7a6ae35e93c176cd8fc7037f30175a65
Jackson <jackson.borba@hotmail.com>
2023-06-13 22:07:28 -0300

    Adicionado read/clear db

--- core/crud.ts
+++ core/crud.ts
@@ -4,9 +4,46 @@ const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
+interface Todo {
+    date: string;
+    content: string;
+    done: boolean;
+}
+
 function create (content: string) {
-    fs.writeFileSync(DB_FILE_PATH, content);
+    const todo: Todo =  {
+        date: new Date().toISOString(),
+        content: content,
+        done: false,
+    };
+
+    const todos: Array<Todo> = [
+        ...read(),
+        todo,
+    ];
+
+    fs.writeFileSync(DB_FILE_PATH, JSON.stringify({
+        todos,
+        dogs: []
+    },null, 2));
     return content;
 }
 
-create("Salvar Dados 21");
+function read(): Array<Todo> {
+    const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+    const db = JSON.parse(dbString || "{}");
+    if (!db.todos) {
+        return [];
+    }
+
+    return db.todos;
+}
+
+function CLEAR_DB() {
+    fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+CLEAR_DB()
+create("First TODO");
+create("Second TODO");
+console.log(read());
