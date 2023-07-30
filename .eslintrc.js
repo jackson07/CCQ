981baee1068b7a810f7a533da41379b1f88a457e
Jackson <jackson.borba@hotmail.com>
2023-07-30 16:45:58 -0300

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,24 @@
 module.exports = {
-    parser: "@typescript-eslint/parser",
-    env: {
-        browser: true,
-        es2021: true,
-        node: true,
-        "cypress/globals": true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    plugins: [
-        "react",
-        "prettier",
-        "@typescript-eslint",
-        "cypress",
-        "chai-friendly",
-        "no-only-tests",
-    ],
-    extends: [
+    "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
         "plugin:react/recommended",
-        "plugin:chai-friendly/recommended",
-        "plugin:cypress/recommended",
-        "plugin:prettier/recommended",
+        "plugin:@typescript-eslint/recommended"
     ],
-    overrides: [],
-    parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
     },
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
-    ignorePatterns: ["node_modules/"],
-    // Cherry of the Cake
-    rules: {
-        "no-only-tests/no-only-tests": "error",
-        "no-console": ["error", { allow: ["warn", "error"] }],
-        "react/no-unknown-property": ["error", { ignore: ["jsx", "global"] }],
-    },
-};
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+}
