d8d73e17dd27bc66d58a950866ee6bc5e038eff4
Jackson <jackson.borba@hotmail.com>
2023-07-31 22:47:35 -0300

    ajustado

--- cypress/e2e/todo-feed.cy.ts
+++ cypress/e2e/todo-feed.cy.ts
@@ -1,5 +1,5 @@
+const base_URL = "http://localhost:3000";
 describe("/ - Todos Feed", () => {
-    const base_URL = "http://localhost:3000";
     it("when load, renders the page,", () => {
         cy.visit(base_URL);
     });
