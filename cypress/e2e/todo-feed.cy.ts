31f6b327d1a64d20380a507f61123ee92daa232f
Jackson <jackson.borba@hotmail.com>
2023-08-30 22:30:47 -0300

    update

--- cypress/e2e/todo-feed.cy.ts
+++ cypress/e2e/todo-feed.cy.ts
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://crudcomqualidade-iota.vercel.app";
 
 describe("/ - Todos Feed", () => {
     it("when load, renders the page", () => {
